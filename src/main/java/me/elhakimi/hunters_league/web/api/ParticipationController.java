e2e8c1f7e9fa8b590cb8cdc7603f60a11776f2b8
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-05 22:21:42 +0100

    refactor code 2

--- src/main/java/me/elhakimi/hunters_league/web/api/ParticipationController.java
+++ src/main/java/me/elhakimi/hunters_league/web/api/ParticipationController.java
@@ -18,7 +18,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
