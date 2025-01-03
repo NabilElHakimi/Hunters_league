03374af903f9e888b647f6e7a5945967aa4e472a
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-01-03 10:13:26 +0100

    change delay of jwt to 1 week

--- src/main/java/me/elhakimi/hunters_league/util/JwtUtil.java
+++ src/main/java/me/elhakimi/hunters_league/util/JwtUtil.java
@@ -57,7 +57,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7)) // 1 week
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
